9c545a8ebd2a3e5edeb412290d38ff4dc0f63792
jhalakmaheshwari <maheshwari.jh@husky.neu.edu>
2017-05-14 20:52:33 -0400

    app change1

--- test/app.js
+++ test/app.js
@@ -8,7 +8,7 @@ module.exports = function(app)
     var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
 
     if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-        var username = process.env.MLAB_USERNAME_WEBDEv; // get from environment
+        var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
         var password = process.env.MLAB_PASSWORD_WEBDEV;
         connectionString = 'mongodb://' + username + ':' + password;
         connectionString += '@ds137261.mlab.com:37261/heroku_3lk683ts'; // user yours
